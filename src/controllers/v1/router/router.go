c297022e810166efeeacfd494da701897b3b03e5
Bagus Al-Qodri <bagus05101994@gmail.com>
2019-04-06 08:18:48 +0700

    create logging handlers using gorilla

--- src/controllers/v1/router/router.go
+++ src/controllers/v1/router/router.go
@@ -3,11 +3,20 @@ package router
 import (
 	"learning-golang/pkg/type/routes"
 	StatusHandler "learning-golang/src/controllers/v1/status"
+	"log"
 	"net/http"
 )
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("X-App-Token")
+		if len(token) < 1 {
+			http.Error(w, "Not authorized", http.StatusUnauthorized)
+			return
+		}
+
+		log.Println("Inside v1 Middleware")
+
 		next.ServeHTTP(w, r)
 	})
 }
--- src/system/app/app.go
+++ src/system/app/app.go
@@ -3,10 +3,13 @@ package app
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"learning-golang/src/system/router"
 
 	"github.com/go-xorm/xorm"
+	"github.com/gorilla/handlers"
 )
 
 type Server struct {
@@ -32,5 +35,25 @@ func (s *Server) Start() {
 
 	r.Init()
 
-	http.ListenAndServe(s.port, r.Router)
+	/*
+	*	Membuat function untuk logging Request Method & Header ketika server dijalankan
+	 */
+	handler := handlers.LoggingHandler(os.Stdout, handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Origin", "Cache-Control", "X-App-Token"}),
+		handlers.ExposedHeaders([]string{""}),
+		handlers.MaxAge(1000),
+		handlers.AllowCredentials(),
+	)(r.Router))
+	handler = handlers.RecoveryHandler(handlers.PrintRecoveryStack(true))(handler)
+
+	newServer := &http.Server{
+		Handler:      handler,
+		Addr:         "0.0.0.0" + s.port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(newServer.ListenAndServe())
 }
